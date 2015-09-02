079a675ef2d81aec0eb888476537659139b97fb1
Costel Radulescu <costel.radulescu.cs@gmail.com>
2015-09-02 10:51:27 +0300

    trimmed dead weight

--- destory_all_vms.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/costel_radulescu/work/horton-works/workspace/ambari-vagrant
-OSes=( "centos5.8" "centos5.9" "centos6.4" "centos6.5" "ubuntu12.4" "win7" "trunk_centos" "trunk_ubuntu" "suse11" )
-for dir in ${OSes[@]}; do
-       	cd $dir 
-	vagrant destroy -f
-	cd ..
-done
--- setup-cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-path="/opt/ambari-vagrant/centos6.4/"
-cd $path
-vagrant destroy -f
-vagrant up c6401 c6402 c6403
-provision-vms.sh $1
